bcfb94cfdec9d0093bf476f95a6730130cac881e
Stephen Adhikurnia <stephen.535200056@stu.untar.ac.id>
2021-11-09 01:22:13 +0700

    Tugas 10

---
+++ TUGAS10_535200056_Stephen/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require('express-session'); 
+const layouts = require('express-ejs-layouts'); 
+
+const app = express();
+
+// use layouts 
+app.use(layouts); 
+app.set('layout', 'layouts/main.ejs'); 
+
+// place all styles block in the layout at the head 
+app.set("layout extractStyles", true) 
+
+// place all scripts block in the layout at the end 
+app.set("layout extractScripts", true)
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// body-parser to parse request body
+app.use(bodyParser.urlencoded());
+
+// static files
+app.use(express.static("public"));
+
+// enabling session 
+
+app.use(session({ 
+    secret: 'some_secret_key',   
+    cookie: {}   
+  })); 
+
+// routes
+const auth = require('./routes/auth'); 
+app.use('/auth', auth); 
+
+const index = require('./routes/index'); 
+app.use('/', index); 
+
+const todo = require('./routes/todo'); 
+app.use('/todo', todo); 
+
+//start server on pot 3000
+app.listen(3000);
+console.log("Server runs at port 3000...");
